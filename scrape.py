cf79d31e5487d34369fd46a0aade47410b63d102
Brent Westbrook <bwestbro@stedwards.edu>
2018-03-28 22:22:12 -0500

    Did something maybe

--- scrape.py
+++ scrape.py
@@ -1,7 +1,13 @@
-from lxml import html
-import requests
-page = requests.get("http://mlb.mlb.com/stats/sortable.jsp#elem=%5Bobject+Object%5D&tab_level=child&click_text=Sortable+Player+hitting&game_type='S'&season=&season_type=SINGLE&league_code='MLB'&sectionType=sp&statType=hitting&page=1&ts=1522041978734&split=&playerType=ALL&timeframe=&active_sw=")
-tree = html.fromstring(page.content)
-help(html)
-for i in tree:
-	print i
+from bs4 import BeautifulSoup
+import urllib2
+
+page_url = "http://mlb.mlb.com/stats/sortable.jsp#elem=%5Bobject+Object%5D&tab_level=child&click_text=Sortable+Player+hitting&game_type='R'&season=2017&season_type=ANY&league_code='MLB'&sectionType=sp&statType=hitting&page=1&ts=1521667465700&playerType=QUALIFIER&sportCode='mlb'&split=&team_id=&active_sw=&position=&page_type=SortablePlayer&sortOrder='desc'&sortColumn=avg&results=&perPage=50&timeframe=&last_x_days=&extended=0"
+
+page = urllib2.urlopen(page_url)
+
+soup = BeautifulSoup(page, 'html.parser')
+
+name_box = soup.find_all('div', attrs={'id': 'datagrid'})
+
+position = soup.find('td', attrs={'class': 'dg-g'})
+print position
