60e784ff394541ee211791f88df9db33e717a4c8
ntBre <bwestbro@stedwards.edu>
2018-03-29 22:59:05 -0500

    Printing what I do have, still does nothing else.

--- scrape.py
+++ scrape.py
@@ -3,11 +3,16 @@ import urllib2
 
 page_url = "http://mlb.mlb.com/stats/sortable.jsp#elem=%5Bobject+Object%5D&tab_level=child&click_text=Sortable+Player+hitting&game_type='R'&season=2017&season_type=ANY&league_code='MLB'&sectionType=sp&statType=hitting&page=1&ts=1521667465700&playerType=QUALIFIER&sportCode='mlb'&split=&team_id=&active_sw=&position=&page_type=SortablePlayer&sortOrder='desc'&sortColumn=avg&results=&perPage=50&timeframe=&last_x_days=&extended=0"
 
-page = urllib2.urlopen(page_url)
+html = urllib2.urlopen(page_url)
+soup = BeautifulSoup(html.read(), 'html.parser')
+for tr in soup.find_all('tr'):
+	print tr
+	tds = tr.find_all('td')
+	print tds[tr].text, "cry"
 
-soup = BeautifulSoup(page, 'html.parser')
+print len(soup.find_all('tr'))
+for line in html:
 
-name_box = soup.find_all('div', attrs={'id': 'datagrid'})
-
-position = soup.find('td', attrs={'class': 'dg-g'})
-print position
+	print html.read()
+print len(html.read())
+print str(soup)
